d1395b412a472e0faeff450e94182e53634d3c67
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-04 23:32:23 +0800

    All functions ready to go. All that's left is adding subreddits

--- src/index.js
+++ src/index.js
@@ -1,29 +1,86 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
 
 
 
-function Generation(){
+function Generation(props){
 
-  return <div>Generation</div>
+  return <div>
+    <form onSubmit={props.generateMeme}>
+    <select id="select" value={props.subreddit} onChange={props.handleChange}>
+      <option value="">Random (Default)</option>
+      <option value="memes">memes</option>
+      <option value="dankmemes">dankmemes</option>
+      <option value="me_irl">me_irl</option>
+      <option value="bonehurtingjuice">bonehurtingjuice</option>
+      <option value="okbuddyretard">okbuddyretard</option>
+      <option value="shitposting">shitposting</option>
+      <option value="PrequelMemes">PrequelMemes</option>
+      <option value="comedyheaven">comedyheaven</option>
+    </select>
+    <input type="submit" value="Generate"/>
+    </form>
+  </div>
 
 }
 
 
-function Meme(){
+function Meme(props){
 
-  return <div>Meme</div>
+  return <div id="memeContainer">
+    <h2>{props.memeTitle}</h2>
+    <p>{props.subredditText}<br/>
+    {props.descriptionText}<br/>
+    <a href={props.originalLink}>{props.originalLink}</a></p>
+    <img src="" alt="Meme" ref={props.imgRef}></img>
+  </div>
 
 }
 
 
 function App(){
-  return <div>
+
+  let [subreddit, setSubreddit] = useState("");
+  const handleChange = e => setSubreddit(e.target.value);
+
+  let [memeTitle, setMemeTitle] = useState("");
+
+  let [descriptionText, setDescriptionText] = useState("");
+  let [subredditText, setSubredditText] = useState("");
+  let [originalLink, setOriginalLink] = useState("");
+
+  let imgRef = useRef();
+
+  function generateMeme(e){
+    e.preventDefault();
+
+    let apiUrl = "https://meme-api.herokuapp.com/gimme";
+
+    apiUrl = subreddit==="" ? apiUrl : apiUrl + `/${subreddit}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+      imgRef.current.src = data.url;
+      setMemeTitle(data.title);
+      setDescriptionText("Posted by " + data.author);
+      setOriginalLink(data.postLink);
+      setSubredditText(`Freshly picked from ${data.subreddit}`)
+    });
+
+  }
+
+  return <div id="container">
     
-    <Generation/>
-    <Meme/>
+    <Generation 
+    subreddit={subreddit} 
+    handleChange={handleChange} 
+    generateMeme={generateMeme}/>
+
+    <Meme imgRef={imgRef} 
+    memeTitle={memeTitle} 
+    descriptionText={descriptionText}
+    originalLink={originalLink}
+    subredditText={subredditText}/>
     
   </div>
 }
